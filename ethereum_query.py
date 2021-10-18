afd42385c7e6915eb021a7a6be0e1c5667441e2e
Wenting Zhao <88762053+Wentinggg@users.noreply.github.com>
2021-10-17 22:03:48 -0700

    Update ethereum_query.py

--- ethereum_query.py
+++ ethereum_query.py
@@ -41,26 +41,20 @@ def get_block_cost(block_num):
     for tx in transactionArray:
 #         print("tx = " + str(tx))
         block_cost += get_transaction_cost(tx)
-    print("target: " + str(get_most_expensive_transaction(10237208)))
     return block_cost
 
 # Return the hash of the most expensive transaction
 def get_most_expensive_transaction(block_num):
-    transactionArray = w3.eth.get_block(10237208)["transactions"]
+    transactionArray = w3.eth.get_block(block_num)["transactions"]
     max_cost = 0
     for tx in transactionArray:
 #         print("tx = " + str(tx))
         if max_cost < get_transaction_cost(tx):
             max_tx = tx
             max_cost = get_transaction_cost(tx)
-#     max_tx = HexBytes(max_tx)  #YOUR CODE HERE
+    max_tx = HexBytes(max_tx)  #YOUR CODE HERE
     return max_tx
     
-    
-# #     for i in range(10237100, 10237103):
-#     thisone = get_block_cost(10237208)/ 10 ** 18
-#     print("thisone: i = " + str(10237208) + " cost: " + str(thisone))
-# #     totalcost += thisone
-# #     print("Cost: " + str(totalcost))
+   
     
     
